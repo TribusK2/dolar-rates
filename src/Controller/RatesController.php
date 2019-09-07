bdb930296cbbb1a32e730ec0f91cc4fc5cdc3b80
TribusK2 <biuro@tribusk2.pl>
2019-09-07 11:58:35 +0200

    Get data from API and render on table

--- src/Controller/RatesController.php
+++ src/Controller/RatesController.php
@@ -14,11 +14,29 @@ class RatesController extends AbstractController
      */
     public function index(Request $request)
     {
-        $currencyNBP = file_get_contents("http://api.nbp.pl/api/exchangerates/rates/c/usd/2019-08-04/2019-09-04/?format=json");
-        $data = json_decode($currencyNBP);
-        dump($data);
+        $date = date("Y-m-d");
+        if ($request->isMethod('POST')){
+            // get data from submit form
+            $date = $request->get('date');
+            dump($date);
+
+            // set query to API
+            $today = date("Y-m-d");
+            dump($today);
+            $query = "http://api.nbp.pl/api/exchangerates/rates/c/usd/".$date."/".$today."/?format=json";
+            dump($query);
+            // send query to API
+            $apiRequest = file_get_contents($query);
+            $rates = json_decode($apiRequest);
+            dump($rates);
+        }
+        // dump($date);
+
         return $this->render('rates/index.html.twig', [
             'controller_name' => 'RatesController',
+            'date' => $date,
+            'rates' => $rates->rates,
+            'code' => $rates->code,
         ]);
     }
 }
