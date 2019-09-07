9ad86de306efa4c5c16ff3b8875adf9c54f6c84a
TribusK2 <biuro@tribusk2.pl>
2019-09-07 10:35:26 +0200

    Added dump() and tested data from API

--- src/Controller/RatesController.php
+++ src/Controller/RatesController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class RatesController extends Controller
+class RatesController extends AbstractController
 {
     /**
      * @Route("/rates", name="rates")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $currencyNBP = file_get_contents("http://api.nbp.pl/api/exchangerates/rates/c/usd/2019-08-04/2019-09-04/?format=json");
+        $data = json_decode($currencyNBP);
+        dump($data);
         return $this->render('rates/index.html.twig', [
             'controller_name' => 'RatesController',
         ]);
