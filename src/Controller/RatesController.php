71f909b57b31b832c03b0e1e101ad5a25b6b06d2
TribusK2 <biuro@tribusk2.pl>
2019-09-07 12:18:41 +0200

    Add calculation of rates

--- src/Controller/RatesController.php
+++ src/Controller/RatesController.php
@@ -14,17 +14,17 @@ class RatesController extends AbstractController
      */
     public function index(Request $request)
     {
+        // data from API
         $date = date("Y-m-d");
         if ($request->isMethod('POST')){
+
             // get data from submit form
             $date = $request->get('date');
-            dump($date);
 
             // set query to API
             $today = date("Y-m-d");
-            dump($today);
             $query = "http://api.nbp.pl/api/exchangerates/rates/c/usd/".$date."/".$today."/?format=json";
-            dump($query);
+
             // send query to API
             $apiRequest = file_get_contents($query);
             $rates = json_decode($apiRequest);
@@ -32,11 +32,32 @@ class RatesController extends AbstractController
         }
         // dump($date);
 
+        // calculation of rate
+        $count = sizeof($rates->rates);
+        $startDate = $rates->rates[0]->effectiveDate;
+        $endDate = $rates->rates[$count-1]->effectiveDate;
+
+        $startAsk = $rates->rates[0]->ask;
+        $endAsk = $rates->rates[$count-1]->ask;
+
+        $startBid = $rates->rates[0]->bid;
+        $endBid = $rates->rates[$count-1]->bid;
+
+        $ask = $endAsk - $startAsk;
+        $bid = $endBid - $startBid;
+
+        dump($ask);
+        dump($bid);
+
         return $this->render('rates/index.html.twig', [
             'controller_name' => 'RatesController',
             'date' => $date,
             'rates' => $rates->rates,
             'code' => $rates->code,
+            'startDate' => $startDate,
+            'endDate' => $endDate,
+            'ask' => $ask,
+            'bid' => $bid,
         ]);
     }
 }
